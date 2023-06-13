e650516ac58cdf4b7345748d12d01aa688c0b8e9
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-06-13 22:13:13 +0100

    added fuctionality to delete item on click/tap

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {
 	ref,
 	push,
 	onValue,
+	remove,
 } from "https:///www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 const appSettings = {
 	databaseURL:
@@ -13,13 +14,16 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const shoppingListInDB = ref(database, "shoppingList");
 const inputField = document.querySelector("#input-field");
+const errorMessage = document.querySelector("span");
 const addButton = document.querySelector("#add-button");
-// const shoppingCart = document.querySelector("#shopping-cart");
 
 const shoppingList = document.querySelector("#shopping-list");
 //function to clear input field
 function clearInputField() {
 	inputField.value = "";
+}
+function error(message) {
+	return message;
 }
 
 //eventlistener on click of button to add item to list
@@ -28,19 +32,49 @@ addButton.addEventListener("click", function () {
 	// clear input field after each button click
 	clearInputField();
 	//add inputValue to list database:
-	push(shoppingListInDB, inputValue);
+	if (inputValue.length > 0) {
+		push(shoppingListInDB, inputValue);
+		errorMessage.textContent = "";
+	} else {
+		errorMessage.textContent = `${error("Input cannot be empty")}`;
+	}
 });
 
-//onValue function to get the shopping List object from the datatbase and convert it to an array
+//onValue function to get the groceryList array having both id and value from the datatbase.
+//onValue allows the live update of the database in real time.
 onValue(shoppingListInDB, function (snapshot) {
-	const groceryListArray = Object.values(snapshot.val());
-	shoppingList.innerHTML = " ";
-	for (let i = 0; i < groceryListArray.length; i++) {
-		appendListItem(groceryListArray[i]);
+	//Check if there is an item in the shopping list
+	if (snapshot.exists()) {
+		const groceryListArray = Object.entries(snapshot.val());
+
+		clearShoppingList();
+
+		for (let i = 0; i < groceryListArray.length; i++) {
+			let currentItem = groceryListArray[i];
+			console.log(currentItem);
+			// current item displays an array having two iems, the id and item text, to get the list to display only the item text, we get the current value from the array.
+			let currentItemValue = currentItem[1];
+			appendListItem(currentItem);
+		}
+	} else {
+		shoppingList.textContent = "No items in cart yet...";
 	}
 });
 
 //add item to shopping cart
 function appendListItem(item) {
-	shoppingList.innerHTML += `<li>${item}</li>`;
+	let itemID = item[0];
+	let itemValue = item[1];
+	const shoppingListItem = document.createElement("li");
+	shoppingListItem.textContent = `${itemValue}`;
+
+	shoppingList.appendChild(shoppingListItem);
+	shoppingListItem.addEventListener("click", function () {
+		const exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+		remove(exactLocationOfItemInDB);
+	});
+}
+
+function clearShoppingList() {
+	shoppingList.innerHTML = " ";
 }
